fe385dfe24f47b0d4a31f2cf44231797106b4400
Mikel Arrieta Etxaniz <ir.mikel.arrieta.etxaniz@gmail.com>
2021-09-09 23:07:41 +0200

    Finished 08 - Firebase Setup.

---
+++ lib/firebase.ts
@@ -0,0 +1,22 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+
+const firebaseConfig = {
+   apiKey: "AIzaSyAcx4Zej5RPkOjLLxUH_GFLjWllaVNh09E",
+   authDomain: "fireship-nextjs-course-fbba8.firebaseapp.com",
+   projectId: "fireship-nextjs-course-fbba8",
+   storageBucket: "fireship-nextjs-course-fbba8.appspot.com",
+   messagingSenderId: "447854776166",
+   appId: "1:447854776166:web:653e0f7b6c64a536e902a7",
+   measurementId: "G-9MH2XPSNXZ"
+ };
+
+ if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+ }
+
+ export const auth = firebase.auth();
+ export const firestore = firebase.firestore();
+ export const storage = firebase.storage();
