ab1d732098bc88099b30c12aa59ebc4755278288
Mikel Arrieta Etxaniz <ir.mikel.arrieta.etxaniz@gmail.com>
2021-09-09 23:43:19 +0200

    Finished 12 - Toast.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
-
-import Loader from '../components/Loader';
+import toast from 'react-hot-toast';
 
 export default function Home() {
 	return (
 		<div>
-			<Loader show={true} />
+			<button onClick={() => toast.success('hello toast!')}>
+				Toast Me
+			</button>
 		</div>
 	);
 }
