14935faf01d93e9cc49aaf9aa3b26cda8f3487e3
Mikel Arrieta Etxaniz <ir.mikel.arrieta.etxaniz@gmail.com>
2021-09-09 23:27:29 +0200

    Finished 10 - Loader.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,12 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
+import Loader from '../components/Loader';
+
 export default function Home() {
 	return (
 		<div>
-			<Link prefetch={false} href={{
-				pathname: '/[username]',
-				query: { username: 'mikel14' },
-			}}>
-				<a>Mikel's profile</a>
-			</Link>
+			<Loader show={true} />
 		</div>
 	);
 }
