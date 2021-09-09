6a64411dcbcab74395c2661aa8718cb9c89a7f9c
Mikel Arrieta Etxaniz <ir.mikel.arrieta.etxaniz@gmail.com>
2021-09-09 23:23:37 +0200

    Finished 09 - Routing.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,16 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
-  return (
-    <div>
-      
-    </div>
-  )
+	return (
+		<div>
+			<Link prefetch={false} href={{
+				pathname: '/[username]',
+				query: { username: 'mikel14' },
+			}}>
+				<a>Mikel's profile</a>
+			</Link>
+		</div>
+	);
 }
