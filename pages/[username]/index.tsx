14cf8122f8d189ba60bb2e42dab34ddf395e8c5f
Mikel Arrieta Etxaniz <ir.mikel.arrieta.etxaniz@gmail.com>
2021-09-17 00:17:15 +0200

    Finished 20 - SSR User Profile Page

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,7 +1,36 @@
-export default function Username({ }) {
+import UserProfile from "../../components/UserProfile";
+import PostFeed from "../../components/PostFeed";
+import { getUserWithUsername, postToJSON } from "../../lib/firebase";
+
+export async function getServerSideProps({ query }) {
+   const { username } = query;
+   
+   const userDoc = await getUserWithUsername(username);
+
+   // JSON serializable data
+   let user = null;
+   let posts = null;
+
+   if (userDoc) {
+      user = userDoc.data();
+      const postsQuery = userDoc.ref
+         .collection("posts")
+         .where("published", "==", true)
+         .orderBy("createdAt", "desc")
+         .limit(5);
+      posts = (await postsQuery.get()).docs.map(postToJSON);
+   }
+
+   return {
+      props: { user, posts }, // will be pased to the page component as props
+   };
+}
+
+export default function Username({ user, posts }) {
    return (
-      <div>
-         
-      </div>
-   )
+      <main>
+         <UserProfile user={user} />
+         <PostFeed posts={posts} admin={null} />
+      </main>
+   );
 }
