db5a51e2641d4986832ad50b1d5c8eb4d8ab96b3
Mikel Arrieta Etxaniz <ir.mikel.arrieta.etxaniz@gmail.com>
2021-10-09 12:40:18 +0200

    Finished 23 - Metatags for SEO

--- pages/admin/[slug].tsx
+++ pages/admin/[slug].tsx
@@ -1,7 +1,11 @@
+import MetaTags from "../../components/Metatags"
+
 export default function AdminPostEdit({ }) {
    return (
       <main>
+         <MetaTags title="admin page" description="admin page" image={null} />
          <h1>Edit Post</h1>
       </main>
-   )
+   );
 }
+ 
