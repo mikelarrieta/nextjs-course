b6971291a6a29072c03dad61ebc780c41c155b52
Mikel Arrieta Etxaniz <ir.mikel.arrieta.etxaniz@gmail.com>
2021-10-09 12:08:29 +0200

    Finished 22 - ISR Incremental Static Regeneration

--- pages/[username]/[slug].tsx
+++ pages/[username]/[slug].tsx
@@ -1,7 +1,50 @@
-export default function PostPage({ }) {
+import styles from '@styles/Post.module.css';
+import PostContent from '@components/PostContent';
+import { firestore, getUserWithUsername, postToJSON } from '../../lib/firebase';
+import { useDocumentData } from 'react-firebase-hooks/firestore';
+
+
+export async function getStaticProps({ params }) {
+   const { username, slug } = params;
+   const userDoc = await getUserWithUsername(username);
+
+   let post;
+   let path;
+
+   if (userDoc) {
+      const postRef = userDoc.ref.collection('posts').doc(slug);
+      post = postToJSON(await postRef.get());
+
+      path = postRef.path;
+   }
+
+   return {
+      props: { post, path },
+      revalidate: 5000,
+   };
+}
+
+export default function PostPage(props) {
    return (
       <main>
          
       </main>
-   )
+   );
+}
+
+export async function getStaticPaths() {
+   // Improve my using Admin SDK to select empty docs
+   const snapshot = await  firestore.collectionGroup('posts').get();
+   
+   const paths = snapshot.docs.map((doc) => {
+      const { slug, username } = doc.data();
+      return {
+         params: { username, slug },
+      };
+   });
+
+   return {
+      paths,
+      fallback: 'blocking',
+   };
 }
